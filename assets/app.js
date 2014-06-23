616b731058651434f235a2cd1fc091c87f81830f
Saif "WJ" Anam <saifanam@users.noreply.github.com>
2014-06-23 18:09:52 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -53,7 +53,7 @@ $(function () {
 	  $('.js-marker-click').smallworld({
 	    geojson: data,
 	    center: [0, 150],
-	    marker: [40.7028, -73.957031],
+	    marker: [17.421216, 78.431182],
 	    markerSize: 8,
   	    markerColor: "#ffc81f",
 	    markerCallback: { selector: ".js-marker-click" }
